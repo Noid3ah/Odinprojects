2d137ab62f63ea0282a6682f81bde8ce4a8476f7
Noid3ah <91058949+Noid3ah@users.noreply.github.com>
2023-11-13 08:43:31 -0500

    fixed a bug with the sidebar not resetting to 0px after transform

--- dashboard/src/script/app.js
+++ dashboard/src/script/app.js
@@ -3,21 +3,21 @@ const dashboard = document.querySelector(".sidebar");
 
 let closed = true;
 
-window.addEventListener("resize", () => {
-  const width = window.innerWidth;
-  console.log(width);
-  if (width > "850px") {
-    dashboard.style.backgroundColor = "#555";
-  }
-});
-
 menu.addEventListener("click", () => {
   if (closed) {
-    dashboard.style.transform = "translateX(0px)";
+    dashboard.classList.remove("move");
     dashboard.style.transition = "transform .4s ease";
   } else {
-    dashboard.style.transform = "translateX(400px)";
+    dashboard.classList.add("move");
     dashboard.style.transition = "transform .2s ease-out";
   }
   closed = !closed;
 });
+
+// window.addEventListener("resize", () => {
+//   const width = window.innerWidth;
+//   console.log(width);
+//   if (width > "850px") {
+//     dashboard.style.backgroundColor = "#555";
+//   }
+// });
