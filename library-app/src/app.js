6e87a9b46b0f3adb556b3afaf5b54b1d4335cf4c
Noid3ah <91058949+Noid3ah@users.noreply.github.com>
2023-11-24 20:05:44 -0500

    Get user input from form and push it to array

--- library-app/src/app.js
+++ library-app/src/app.js
@@ -1,22 +1,53 @@
 const openButton = document.querySelector("[data-open-modal]");
-const closeButton = document.querySelector("[data-close-modal]");
+const submitAndClose = document.querySelector("[data-close-modal]");
+const cancelButton = document.querySelector("[data-cancel-modal]");
+
 const modal = document.querySelector("[data-modal]");
 
+const form = document.getElementById("book-form");
+const title = form.querySelector("#title");
+const author = form.querySelector("#author");
+const pages = form.querySelector("#pages");
+const checkbox = form.querySelector("#read");
+
+function resetFields() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  checkbox.checked = false;
+}
 openButton.addEventListener("click", () => {
   modal.showModal();
 });
 
-closeButton.addEventListener("click", (e) => {
-  e.preventDefault();
+cancelButton.addEventListener("click", () => {
   modal.close();
+  resetFields();
+});
+
+submitAndClose.addEventListener("click", (e) => {
+  // e.preventDefault();
+  if (!author.value || !title.value || !pages.value) return;
+
+  addBookToLibrary(
+    new Book(title.value, author.value, pages.value, checkbox.checked)
+  );
+  modal.close();
+  resetFields();
 });
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = Boolean(read);
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(myLibrary);
 }
+
+console.log(myLibrary);
