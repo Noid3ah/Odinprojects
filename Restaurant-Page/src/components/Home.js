3a2e4474392915dc2face30457626a966732fa03
Noid3ah <91058949+Noid3ah@users.noreply.github.com>
2024-02-24 13:16:27 -0500

    add an order button

--- Restaurant-Page/src/components/Home.js
+++ Restaurant-Page/src/components/Home.js
@@ -2,11 +2,14 @@ export default function Home() {
   const element = document.createElement('div');
   const heroText = document.createElement('h1');
   const heroSpan = document.createElement('span');
-  element.className = 'hero';
+  const cta = document.createElement('button');
+
+  element.className = 'hero contain';
   heroText.className = 'inter-x';
   heroSpan.className = 'dress';
   heroText.textContent = 'Good food is the foundation of genuine happiness';
   heroSpan.textContent = '"Food is a passion, food is love"';
-  element.append(heroText, heroSpan);
+  cta.textContent = 'Order now';
+  element.append(heroText, heroSpan, cta);
   return element;
 }
